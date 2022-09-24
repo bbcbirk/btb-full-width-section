1dd5511382ae190d6798b5ca2301cc690c213b62
Birk Thestrup Blauner <birkblauner@gmail.com>
2022-09-24 22:45:55 +0200

    Update version

--- btb-full-width-section.php
+++ btb-full-width-section.php
@@ -4,7 +4,7 @@
  * Description:       Full width Section block.
  * Requires at least: 6.0
  * Requires PHP:      7.0
- * Version:           v2.0.0
+ * Version:           2.0.0
  * Author:            birkblauner
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://wordpressfoundation.org/donate/
 Tags: block, design
 Requires at least: 6.0
 Tested up to: 6.0
-Stable tag: v2.0.0
+Stable tag: 2.0.0
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
